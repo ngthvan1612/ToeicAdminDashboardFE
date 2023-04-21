5ecb4707265d236235f5f79a87b8327e3ebef149
ngthvan1612@gmail.com <ngthvan1612@gmail.com>
2023-04-22 00:11:31 +0700

    Init CORE UI

---
+++ src/views/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import WidgetsBrand from '../widgets/WidgetsBrand'
+import WidgetsDropdown from '../widgets/WidgetsDropdown'
+
+const Dashboard = () => {
+  return (
+    <>
+      <h1>Dashboard Toeic</h1>
+    </>
+  )
+}
+
+export default Dashboard
