7494a4cc5344fa97bcfa18c201ab3b1eacb6c915
qhuong1008 <qhuong1008@gmail.com>
2023-04-23 12:00:25 +0700

    route

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React from "react";
 
-const ToeicFullTest = React.lazy(() => import('./views/tests/ToeicFullTest'))
-const CreateToeicFullTest = React.lazy(() => import('./views/tests/CreateToeicFullTest'))
-const UpdateToeicFullTest = React.lazy(() => import('./views/tests/UpdateToeicFullTest'))
+const ToeicFullTest = React.lazy(() => import("./views/tests/ToeicFullTest"));
+const CreateToeicFullTest = React.lazy(() =>
+  import("./views/tests/CreateToeicFullTest")
+);
+const UpdateToeicFullTest = React.lazy(() =>
+  import("./views/tests/UpdateToeicFullTest")
+);
+const Witgets = React.lazy(() => import("./views/widgets/Widgets"));
+const WitgetsBrand = React.lazy(() => import("./views/widgets/WidgetsBrand"));
+const WitgetsDropdown = React.lazy(() =>
+  import("./views/widgets/WidgetsDropdown")
+);
 
-const ToeicFullTestViewListParts = React.lazy(() => import('./views/tests/ToeicFullTestViewListParts'))
+const ToeicFullTestViewListParts = React.lazy(() =>
+  import("./views/tests/ToeicFullTestViewListParts")
+);
 
 const toeicRoutes = [
-  { path: '/', exact: true, name: 'Home' },
-  { path: '/test-manager/tests', element: ToeicFullTest },
-  { path: '/test-manager/tests/create', element: CreateToeicFullTest },
-  { path: '/test-manager/tests/update/:toeicFullTestId', element: UpdateToeicFullTest },
-  { path: '/test-manager/tests/:toeicFullTestId', element: ToeicFullTestViewListParts }
-]
+  { path: "/", exact: true, name: "Home" },
+  { path: "/test-manager/tests", element: ToeicFullTest },
+  { path: "/test-manager/tests/create", element: CreateToeicFullTest },
+  {
+    path: "/test-manager/tests/update/:toeicFullTestId",
+    element: UpdateToeicFullTest,
+  },
+  {
+    path: "/test-manager/tests/:toeicFullTestId",
+    element: ToeicFullTestViewListParts,
+  },
+  { path: "/test-manager/statistics", element: Witgets },
+  { path: "/test-manager/statistics/brand", element: WitgetsBrand },
+  { path: "/test-manager/statistics/dropdown", element: WitgetsDropdown },
+];
 
-export default toeicRoutes
+export default toeicRoutes;
