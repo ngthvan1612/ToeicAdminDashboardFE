10c54ebc0346375ba235a8b3f52d3e19f650854e
Nguyễn Thành Văn <ngthvan1612@gmail.com>
2023-05-11 22:22:39 +0700

    Update toeicItemContent.js

---
+++ src/api/toeicItemContent.js
@@ -0,0 +1,23 @@
+import { contextInstance } from "./axios";
+
+const createToeicQuestionItem = async ({contentType, stringContent, content, questionContentId, questionTranscriptId}) => {
+  const formData = new FormData();
+
+  formData.append("contentType", contentType);
+  formData.append("stringContent", stringContent);
+  formData.append("content", content);
+  formData.append("questionContentId", questionContentId);
+  formData.append("questionTranscriptId", questionTranscriptId);
+
+  return await contextInstance.post(
+    `api/toeic/toeic-item-content/create-new-item-content`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }
+  );
+};
+
+export { createToeicQuestionItem }
